5d6e92edde68fe6ef05479ac3c7bd2bd0a1b545e
Fabian Ibañez <fabian.i@beclouder.com>
2020-11-15 21:51:37 -0300

    add filter by dominio

--- src/routes/vehicles.js
+++ src/routes/vehicles.js
@@ -6,5 +6,21 @@ router.get('/',(req,res)=>{
     res.send(dataVehicle);
 })
 
+router.get('/:dominio',(req,res)=>{
+    const resp = {
+        "resp": false,
+        "result": []
+    }
+    const dominio = req.params.dominio;
+    let result = dataVehicle.filter(v => v.dominio == dominio);
+
+    if(result.length > 0){
+        resp.resp = true;
+        resp.result = result
+    }
+
+    res.send(resp);
+})
+
 
 module.exports = router;
